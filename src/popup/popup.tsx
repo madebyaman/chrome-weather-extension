3663e99b74e67a396352659c2402c0c743bfdd0b
Aman Thakur <builtbyaman@gmail.com>
2022-06-06 13:21:00 +0530

    getting and storing

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,28 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import './popup.css';
 import { WeatherCard } from './WatherCard';
 import { Grid, InputBase, Paper, IconButton, Box } from '@mui/material';
 import { Add as AddIcon } from '@mui/icons-material';
+import { getStoredCities, storeCities } from '../utils/storage';
 import '@fontsource/roboto';
 
 const App: React.FC<{}> = () => {
-  const [cities, setCities] = useState<string[]>([
-    'Toronto',
-    'New Delhi',
-    'Error',
-  ]);
+  const [cities, setCities] = useState<string[]>([]);
   const [cityInput, setCityInput] = useState<string>('');
 
+  useEffect(() => {
+    getStoredCities().then(setCities);
+  }, [setCities]);
+
   function handleCityButtonClick() {
+    console.log(cityInput);
     if (!cityInput) return;
-    setCities([...cities, cityInput]);
-    setCityInput('');
+    const newCities = [...cities, cityInput];
+    storeCities(newCities).then(() => {
+      setCities(newCities);
+      setCityInput('');
+    });
   }
 
   function handleDelete(cityName: string) {
     const newCities = cities.filter((city) => city !== cityName);
-    setCities(newCities);
+    storeCities(newCities).then(() => {
+      setCities(newCities);
+    });
   }
 
   return (
