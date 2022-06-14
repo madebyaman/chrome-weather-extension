ed54533dae7c09b30cf538f27a035026f0915d15
Aman Thakur <builtbyaman@gmail.com>
2022-06-14 14:21:00 +0530

    context menu

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,6 +1,22 @@
-import { setStoredOptions, storeCities } from '../utils/storage';
+import {
+  getStoredCities,
+  setStoredOptions,
+  storeCities,
+} from '../utils/storage';
 
 chrome.runtime.onInstalled.addListener(() => {
   storeCities([]);
   setStoredOptions({ inCelsius: true, homeCity: '', hasAutoOverlay: false });
+
+  chrome.contextMenus.create({
+    contexts: ['selection'],
+    title: 'Add city to weather',
+    id: 'weatherExtension',
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((e) => {
+  getStoredCities().then((cities) => {
+    storeCities([...cities, e.selectionText]);
+  });
 });
