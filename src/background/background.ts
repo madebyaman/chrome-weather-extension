b6d03c6303693990f61f427925d3d04f3dc24a28
Aman Thakur <builtbyaman@gmail.com>
2022-06-15 14:21:00 +0530

    badge

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,8 +1,10 @@
 import {
   getStoredCities,
+  getStoredOptions,
   setStoredOptions,
   storeCities,
 } from '../utils/storage';
+import { fetchOpenWeatherData } from '../utils/api';
 
 chrome.runtime.onInstalled.addListener(() => {
   storeCities([]);
@@ -18,5 +20,16 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.contextMenus.onClicked.addListener((e) => {
   getStoredCities().then((cities) => {
     storeCities([...cities, e.selectionText]);
+  });
+});
+
+getStoredOptions().then((options) => {
+  if (!options.homeCity) return;
+  fetchOpenWeatherData(options.homeCity, options.inCelsius).then((data) => {
+    const temp = Math.round(data.main.temp);
+    const symbol = options.inCelsius ? '\u2103' : '\u2109';
+    chrome.action.setBadgeText({
+      text: `${temp}${symbol}`,
+    });
   });
 });
